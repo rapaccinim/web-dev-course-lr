047042506ed22fc15806c7bc36dccaece51b55b6
Marco Rapaccini <info@marcorapaccini.it>
2022-01-26 00:29:28 +0000

    Added first React SPA created with create-react-app.

---
+++ first-react-web-app/my-app/src/components/Header/header.js
@@ -0,0 +1,13 @@
+/**
+ * My first component
+ */
+
+ import './style.css';
+
+const Header = () => {
+    return(
+        <div className='headerBar'>This is the header bar</div>
+    )
+}
+
+export default Header;
