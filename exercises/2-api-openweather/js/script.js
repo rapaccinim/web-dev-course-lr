7459c9210da46bac2c7d829bff9e723444433547
Leonardo Russo <leonardo.russo203@gmail.com>
2022-01-26 10:25:14 +0100

    Updated files of the Openweather 2 exercise

--- exercises/2-api-openweather/js/script.js
+++ exercises/2-api-openweather/js/script.js
@@ -2,20 +2,21 @@ const start = () => {
   const cityName = document.getElementById("cityName").value;
   const varColor = document.getElementById("weatherCircle");
   const apiCall = `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${apiKey}`;
-  const response = fetch(apiCall)
+  fetch(apiCall)
     .then(res => res.json())
     .then(res => {
-      let dt1 = new Date((res.sys.sunrise + res.timezone) * 1000)
-      let h1 = (dt1.getHours() - 1)
-      let m1 = "0" + dt1.getMinutes()
-      let t1 = h1 + ":" + m1.substr(-2)
-      document.getElementById("p-div1").innerText = "Sunrise: " + t1;
-
-      let dt2 = new Date((res.sys.sunset + res.timezone) * 1000)
-      let h2 = (dt2.getHours() - 1)
-      let m2 = "0" + dt2.getMinutes()
-      let t2 = h2 + ":" + m2.substr(-2)
-      document.getElementById("p-div2").innerText = "Sunset: " + t2;
+      const getTime = (event) =>{
+        let r = res.sys;
+        let dt = new Date((r[event] + res.timezone) * 1000)
+        let h = (dt.getHours() - 1)
+        let m = "0" + dt.getMinutes()
+        let t = h + ":" + m.substr(-2)
+        return t;
+      }
+      const sunrise = "sunrise";
+      document.getElementById("p-div1").innerText = "Sunrise: " + getTime(sunrise);
+      const sunset = "sunset";
+      document.getElementById("p-div2").innerText = "Sunset: " + getTime(sunset);
 
       let var1 = res["weather"];
       let var2 = var1[0];
