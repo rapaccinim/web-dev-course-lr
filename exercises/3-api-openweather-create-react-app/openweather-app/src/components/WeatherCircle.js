1500a39ccc1738b25df94b9bfb5a7b313ca192a1
Leonardo Russo <leonardo.russo203@gmail.com>
2022-02-01 08:19:12 +0100

    Added files of the Openweather 3 exercise.

---
+++ exercises/3-api-openweather-create-react-app/openweather-app/src/components/WeatherCircle.js
@@ -0,0 +1,14 @@
+const WeatherCircle = () =>{
+  return(
+    <>
+        <div id="weatherCircle"></div>
+        <div id="sunrise">
+          <p id="p-div1">(to be loaded)</p>
+        </div>
+        <div id="sunset">
+          <p id="p-div2">(to be loaded)</p>
+        </div>
+    </>
+  )
+}
+export default WeatherCircle;
