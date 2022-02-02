8736d6ef9328b50418d62402d1448c52f5f4fa43
Marco Rapaccini <info@marcorapaccini.it>
2022-02-02 20:48:41 +0000

    Moved the logic from the App.js to the specific component. Code refactoring for avoiding code duplication

--- exercises/3-api-openweather-create-react-app/openweather-app/src/App.js
+++ exercises/3-api-openweather-create-react-app/openweather-app/src/App.js
@@ -2,47 +2,9 @@ import "./App.css";
 import WeatherCircle from './components/WeatherCircle.js';
 
 function App() {
-  const apiKey = process.env.REACT_APP_NOT_SECRET_CODE;
-  const start = () => {
-    const cityName = document.getElementById("cityName").value;
-    const varColor = document.getElementById("weatherCircle");
-    const apiCall = `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${apiKey}`;
-    fetch(apiCall)
-      .then(res => res.json())
-      .then(res => {
-        const getTime = (event) =>{
-          let r = res.sys;
-          let dt = new Date((r[event] + res.timezone) * 1000)
-          let h = (dt.getHours() - 1)
-          let m = "0" + dt.getMinutes()
-          let t = h + ":" + m.substr(-2)
-          return t;
-        }
-        const sunrise = "sunrise";
-        document.getElementById("p-div1").innerText = "Sunrise: " + getTime(sunrise);
-        const sunset = "sunset";
-        document.getElementById("p-div2").innerText = "Sunset: " + getTime(sunset);
 
-        let var1 = res["weather"];
-        let var2 = var1[0];
-        let var3 = var2["main"];
-        if (var3 == "Clear") {
-          varColor.style.backgroundColor = "#5C7AEA";
-        } else if (var3 == "Clouds") {
-          varColor.style.backgroundColor = "#9D9D9D";
-        } else {
-          varColor.style.backgroundColor = "#EA5C2B";
-        }
-      })
-      .catch(err => console.log("Error! ", err))
-  }
   return (
     <div className="App">
-      <form>
-        <label>Enter the name of the city: </label><br />
-        <input type="text" id="cityName"/><br />
-      </form>
-      <button onClick={start} id="submitButton">OpenWeather Api Call</button><br />
       <WeatherCircle />
     </div>
   );
--- exercises/3-api-openweather-create-react-app/openweather-app/src/components/WeatherCircle.js
+++ exercises/3-api-openweather-create-react-app/openweather-app/src/components/WeatherCircle.js
@@ -1,14 +1,51 @@
-const WeatherCircle = () =>{
-  return(
-    <>
-        <div id="weatherCircle"></div>
-        <div id="sunrise">
-          <p id="p-div1">(to be loaded)</p>
-        </div>
-        <div id="sunset">
-          <p id="p-div2">(to be loaded)</p>
-        </div>
-    </>
+const WeatherCircle = () => {
+
+  const apiKey = process.env.REACT_APP_NOT_SECRET_CODE;
+  const settings = ["sunrise", "sunset"]
+
+  // dummy function
+  const capitalise = (s) => {
+    return s && s[0].toUpperCase() + s.slice(1);
+  }
+
+  const start = () => {
+    const cityName = document.getElementById("cityName").value;
+    const varColor = document.getElementById("weatherCircle");
+    const apiCall = `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${apiKey}`;
+    fetch(apiCall)
+      .then(res => res.json())
+      .then(res => {
+        const getTime = (event) => {
+          const dt = new Date((res.sys[event] + res.timezone) * 1000)
+          return ((dt.getHours() - 1) + ":" + dt.getMinutes())
+        }
+
+        settings.forEach(setting => document.getElementById(setting).innerText = capitalise(setting) + " " + getTime(setting) )
+
+        let var1 = res["weather"];
+        let var2 = var1[0];
+        let var3 = var2["main"];
+        if (var3 == "Clear") {
+          varColor.style.backgroundColor = "#5C7AEA";
+        } else if (var3 == "Clouds") {
+          varColor.style.backgroundColor = "#9D9D9D";
+        } else {
+          varColor.style.backgroundColor = "#EA5C2B";
+        }
+      })
+      .catch(err => console.log("Error! ", err))
+  }
+
+  return (
+    <div>
+      <form>
+        <label>Enter the name of the city: </label><br />
+        <input type="text" id="cityName" /><br />
+      </form>
+      <button onClick={start} id="submitButton">OpenWeather Api Call</button><br />
+      <div id="weatherCircle"></div>
+      {settings.map( setting => <div id={setting}>(to be loaded)</div>)}
+    </div>
   )
 }
 export default WeatherCircle;
